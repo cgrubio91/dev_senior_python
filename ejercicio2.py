7a3a72bfd0f56979a425d34437259f27f3abaf3c
cgrubio91 <104238457+cgrubio91@users.noreply.github.com>
2024-10-24 23:08:32 -0500

    Ejercicio 2

---
+++ ejercicio2.py
@@ -0,0 +1,3 @@
+### **Ejercicio 2: Conversión de Unidades**
+#Crea un programa que convierta una medida en metros a centímetros y milímetros. El programa debe pedir al usuario que ingrese una longitud en metros y luego mostrar el resultado en las dos unidades.
+
