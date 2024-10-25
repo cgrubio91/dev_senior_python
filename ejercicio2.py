d90fc4b684d7467e311f384926faf4bd23768853
cgrubio91 <104238457+cgrubio91@users.noreply.github.com>
2024-10-24 23:37:45 -0500

    Ejercicio #2

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,11 @@
 ### **Ejercicio 2: Conversión de Unidades**
 #Crea un programa que convierta una medida en metros a centímetros y milímetros. El programa debe pedir al usuario que ingrese una longitud en metros y luego mostrar el resultado en las dos unidades.
 
+medidaInicial = float(input("Ingresa una longitud en metros: "))
+
+centimetros = medidaInicial * 100
+milimetros = medidaInicial * 1000
+
+print("Ejercicio 2: Conversión de unidades")
+print(f"\n{medidaInicial} mts es igual a {centimetros} cm")
+print(f"\n{medidaInicial} mts es igual a {milimetros} ml")
