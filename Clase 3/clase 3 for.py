80613f87d07a7ea2e513f16846fe641d4353c9f8
cgrubio91 <cgrubio91@gmail.com>
2024-10-29 22:48:33 -0500

    Fin de la clase 3

--- Clase 3/clase 3 for.py
+++ Clase 3/clase 3 for.py
@@ -1,3 +1,10 @@
 # Este bucle se repite hasta que se cumple la condición,
-for i in range(0,10,2):
-    print(i)
+# for i in range(0,10,2):
+#     print(i)
+
+numero = int(input('ingrese un número: '))
+for numero in range(1, 6):
+    if numero == 3:
+        print('Número 3 encontrado, saliendo del bucle')
+        break
+    print(numero)
