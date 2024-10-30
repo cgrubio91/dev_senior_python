11576f06c7463a2a3a444f4f273c342f85f3b990
cgrubio91 <cgrubio91@gmail.com>
2024-10-29 21:15:35 -0500

    Ejercicio if - elif - else

--- Clase 3/clase 3.py
+++ Clase 3/clase 3.py
@@ -1,2 +1,10 @@
-print("hola")
-print("comit")
+age = 18
+country = 'COL'
+
+if age >= 21 and country == 'USA':
+    print('You can bay alcohol')
+elif age >= 18 and country == 'COL':
+    print('You can bay alcohol you area in COL')
+else:
+    print('You can´t bay alcohol')
+
