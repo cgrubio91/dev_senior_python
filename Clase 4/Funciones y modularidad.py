f9cf3c458679e1570c55d9456162f9671aecdc27
cgrubio91 <cgrubio91@gmail.com>
2024-11-05 22:55:15 -0500

    Ejercicio en clase 4

--- Clase 4/Funciones y modularidad.py
+++ Clase 4/Funciones y modularidad.py
@@ -1,8 +1,15 @@
 #Programa de gestión de drogueria
 ventasTotales = 0.0
+#solicitar el número de productos
+numProductos = int(input('Ingrese el npumero de productos a gestionar: '))
 
 #Listas para almacenar la información del inventario
 
 nombre = []
 precios = []
 cantidades = []
+
+print('Ingreso inicial de productos de la tienda: ')
+for i in range(numProductos):
+    print(f'Producto {i+1}: ')
+    
