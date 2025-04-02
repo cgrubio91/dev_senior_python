361391e830438f6a18922328494f28be2f0fad39
cgrubio91 <cgrubio91@gmail.com>
2025-04-01 21:52:13 -0500

    conexiondb

--- Modulo_DB/conecionSQL.py
+++ Modulo_DB/conecionSQL.py
@@ -11,4 +11,23 @@ conexion = mysql.connector.connect(
 
 if conexion.is_connected():
     print("Conexión exitosa a la base de datos")
-    ##cgrubio91/dev_senior_python/Modulo_DB/conecionSQL.py
+
+# Insertar datos(INSERT)
+query = "INSERT INTO Profesores(nombre,especialidad,experiencia) VALUES(%s, %s, %s);"
+valores = ("Juan David", "Ingenieria de Sistemas", 5)
+cursos.execute(query, valores)
+conexion.commit() # Guardar los cambios
+
+# Actualizar(UPDATE)
+query = "UPDATE profesores SET experiencia = %s WHERE id_profesor = %s"
+valores = (7, 12)  
+cursos.execute(query,valores)
+conexion.commit() # Guardar los cambios
+print(f"Fila insertada: {cursos.rowcount}")
+
+# Eliminar(DELETE)
+query = "DELETE FROM profesores WHERE id_profesor =%s;"
+valores = (14,)  
+cursos.execute(query,valores)
+conexion.commit() # Guardar los cambios
+print(f"Fila insertada: {cursos.rowcount}")
